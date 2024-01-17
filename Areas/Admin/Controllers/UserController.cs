d6f96ac2e368babfa3b7d9502a30bcdc6eff4a68
gnart1 <76728890+gnart1@users.noreply.github.com>
2024-01-17 17:32:42 +0700

    login function

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBlog.Data;
+using WebBlog.Models;
+using WebBlog.ViewModels;
 
 namespace WebBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly INotyfService _notification;
+
+        public UserController(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, INotyfService notification)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _notification = notification;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,7 +30,31 @@ namespace WebBlog.Areas.Admin.Controllers
         [HttpGet("Login")]
         public IActionResult Login()
         {
-            return View();
+            return View(new LoginVM());
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == vm.UserName);
+            if (existingUser == null)
+            {
+                _notification.Error("Tài khoản không tồn tại");
+                return View(vm);
+            }
+            
+            var verifyPassword = await _userManager.CheckPasswordAsync(existingUser, vm.Password);
+            if(!verifyPassword)
+            {
+                _notification.Error("Mật khẩu không đúng");
+                return View(vm);
+            }
+            await _signInManager.PasswordSignInAsync(vm.UserName, vm.Password, vm.RememberMe, true);
+            _notification.Success("Đăng nhập thành công!");
+            return RedirectToAction("Index", "User", new { area = "Admin" });
         }
 
     }
---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace WebBlog.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
