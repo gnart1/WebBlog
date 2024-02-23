edc46cb4a7cc3e7d82db8e9364ad9a3e7b84c4ba
gnart1 <76728890+gnart1@users.noreply.github.com>
2024-02-23 13:33:30 +0700

    tạo funciton create post

--- Areas/Admin/Controllers/PostController.cs
+++ Areas/Admin/Controllers/PostController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBlog.Data;
+using WebBlog.Models;
 using WebBlog.ViewModels;
 
 namespace WebBlog.Areas.Admin.Controllers
@@ -8,6 +13,24 @@ namespace WebBlog.Areas.Admin.Controllers
     [Authorize]
     public class PostController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(ApplicationDbContext context, INotyfService notification,
+            IWebHostEnvironment webHostEnvironment, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _notification = notification;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
+
+
+
+
+
         public IActionResult Index()
         {
             return View();
@@ -18,5 +41,50 @@ namespace WebBlog.Areas.Admin.Controllers
         {
             return View(new PostVM());
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(PostVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+
+            var post = new Post();
+            post.Title = vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+            if(post.Title != null) 
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+
+            if(vm.Image != null)
+            {
+               post.ImageUrl = UploadImage(vm.Image);
+            }
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Tạo bài viết thành công!");
+            return RedirectToAction("Index");
+            
+        }
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "image");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using(FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
+        }
+
     }
 }
--- ViewModels/PostVM.cs
+++ ViewModels/PostVM.cs
@@ -1,10 +1,12 @@
-﻿using WebBlog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WebBlog.Models;
 
 namespace WebBlog.ViewModels
 {
     public class PostVM
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? ApplicationUserId { get; set; }
